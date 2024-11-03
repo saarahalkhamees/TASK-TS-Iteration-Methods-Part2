a13b7133f11e65bd467963fcce3fd2f8d8894ab2
Octowl <alsaff1987@gmail.com>
2024-11-04 01:19:24 +0300

    better descriptions

--- iterationMethods.js
+++ iterationMethods.js
@@ -6,38 +6,37 @@ const transactions = [
   ["income", 700],
 ];
 
-// 1) Using `filterIncomeTransactions` function return a new array containing only the income transactions.
+// 1) Write a `filterIncomeTransactions` function that returns a new array containing only the income transactions.
 function filterIncomeTransactions(transactions) {
   // write your code here...
 }
 
-// 2) Using `filterExpenseTransactions` function return a new array containing only the expense  transactions.
+// 2) Write a `filterExpenseTransactions` function that return a new array containing only the expense transactions.
 function filterExpenseTransactions(transactions) {
   // write your code here...
 }
 
-// 3) Using `calculateTotalIncome` Calculate the total income and return it.
+// 3) Write a `calculateTotalIncome` function that calculates the total income and returns it.
 function calculateTotalIncome(transactions) {
   // write your code here...
 }
 
-// 4) Using `calculateTotalExpenses` Calculate the total expense and return it.
+// 4) Write a `calculateTotalExpenses` function that calculates the total expense and returns it.
 function calculateTotalExpenses(transactions) {
   // write your code here...
 }
 
-// 5) Using `calculateNetTotal` Calculate the net total (total income - total expenses) and return it.
+// 5) Write a `calculateNetTotal` function that calculates the net total (total income - total expenses) and returns it.
 function calculateNetTotal(transactions) {
   // write your code here...
 }
 
-// 6) Using `filterSignificantTransactions` function Identify and create a new array with transactions (both incomes and expenses) above or equal to $500.
+// 6) Write a `filterSignificantTransactions` function that identifies and creates a new array with transactions (both incomes and expenses) above or equal to $500.
 function filterSignificantTransactions(transactions, threshold = 500) {
   // write your code here...
 }
 
 module.exports = {
-  transactions,
   filterIncomeTransactions,
   filterExpenseTransactions,
   calculateTotalIncome,
